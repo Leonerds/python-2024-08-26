23a88bcd1e08541f8242dc42cc6086377e934171
Leonardo Camille <89817225+Leonerds@users.noreply.github.com>
2024-09-29 11:21:40 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,47 @@
+import mysql.connector
 import questionary
 import os
 from rich.console import Console
 from rich.table import Table
 from typing import List
 from modelos import Marca
-# import banco_dados
 from banco_dados import conectar
 
-
-# https://dontpad.com/franciscosensaulas/python
+# CRUD
+# Create
+# Read
+# Update
+# Delete
 
 
 def limpar_tela():
     os.system("cls")
+
+
+def obter_todas_marcas() -> List[Marca]:
+    conexao = conectar()
+    print("Conectado com Sucesso")
+    # criado cursor que nos permitira executar comandos de SQL
+    cursor = conexao.cursor()
+    cursor.execute("SELECT id, nome, cnpj FROM marcas")
+    # fetchall buscar todos os registros encontrados na consulta
+    registros = cursor.fetchall()
+    # fechar a conexão com o banco de dados
+    conexao.close() 
+
+    marcas: List[Marca] = []
+    #percorrer cada um dos registros que consultamos do banco de dados
+    for registro in registros:
+        #obter os valores das colunas do select
+        id = registro[0]
+        nome = registro[1]
+        cnpj = registro[2]
+        # intanciando um objeto da marca, passando como parametro no construtor
+        marca = Marca(id, nome, cnpj)
+        # adicionando a marca na lista de marcas 
+        marcas.append(marca)
+    #retornar lista de marcas
+    return marcas
 
 
 def menu_marcas():
@@ -38,48 +67,18 @@ def menu_marcas():
         elif opcao_escolhida == "Apagar marca":
             apagar_marca()
 
-
 def inserir_marca(): # create
+
     nome = questionary.text("Informe o nome da marca: ").ask()
     cnpj = questionary.text("Informe o CNPJ: ").ask()
-
-    # conexao = banco_dados.conectar()
     conexao = conectar()
-    print("Conectado com sucesso")
-
+    print("Conectado com Sucesso")
     cursor = conexao.cursor()
     cursor.execute("use dev_motors")
     cursor.execute(f"INSERT INTO marcas (nome, cnpj) VALUES ('{nome}', '{cnpj}');")
     conexao.commit() # Efetuar a transação
-    conexao.close() # Fechar a conexão com o banco de dados
+    conexao.close() # Fechar a conexão com o bando de dados
     print("Marca cadastrada com sucesso")
-
-def obter_todas_marcas() -> List[Marca]:
-    conexao = conectar()
-    print("Conectado com sucesso")
-    # criado cursor que nos permitirá executar comandos de SQL 
-    cursor = conexao.cursor()
-    # definir que será executado uma consulta (SELECT) de todas as marcas 
-    cursor.execute("SELECT id, nome, cnpj FROM marcas")
-    # fetchall buscar todos os registros encontrados na consulta
-    registros = cursor.fetchall()
-    # fechar a conexão com o banco de dados
-    conexao.close()
-
-    # criando a lista de marcas
-    marcas : List[Marca] = []
-    # percorrer cada um dos registros que consultamos do banco de dados
-    for registro in registros:
-        # obter os valores das colunas do select
-        id = registro[0]
-        nome = registro[1]
-        cnpj = registro[2]
-        # instanciando um objeto da marca, passando como parâmetro no construtor
-        marca = Marca(id, nome, cnpj)
-        # adicionando a marca na lista de marcas
-        marcas.append(marca)
-    # retornar a lista de marcas
-    return marcas
 
 
 def consultar_marcas(): # read
@@ -95,58 +94,51 @@ def consultar_marcas(): # read
         table.add_row(str(marca.id), marca.nome, marca.cnpj)
 
     console = Console()
-    console.print(table)
-
-    console.print(table)
+    console.print(table)  
 
 
-# https://dontpad.com/franciscosensaulas/python
+def atualizar_marca(): # update
+    marcas = obter_todas_marcas()
+
+    opcoes_para_escolher = []
+    for marca in marcas:
+        opcao = questionary.Choice(title=marca.nome, value=marca.id)
+        opcoes_para_escolher.append(opcao)
+
+        id_marca_escolhida = questionary.select(
+            "Escolha a marca que deseja editar",
+            choices=opcoes_para_escolher
+        ).ask()
+
+        nome_editado = questionary.text("Informe o nome da marca: ").ask()
+        cnpj_editado = questionary.text("Informe o CNPJ: ").ask()
+
+    conexao = conectar()
+    print("Conectado com Sucesso")
+    cursor = conexao.cursor()
+    cursor.execute(f"UPDATE marcas SET nome = '{nome_editado}', cnpj = '{cnpj_editado}' WHERE id = {id_marca_escolhida}")
+    conexao.commit()
+    conexao.close()
+
 def apagar_marca(): # delete
     # Consultar todas as marcas
     marcas = obter_todas_marcas()
-
-    # Criar um vetor com as marcas para o usuário poder escolher a marca que deseja apagar
+    # Criar um vetor com as marcas para o usuario poder escolher a marca que deseja apagar
     opcoes = []
     for marca in marcas:
         opcoes.append(marca.nome)
 
-    # perguntando para o usuário qual marca ele deseja apagar
+    # Perguntando para o usuario qual marca ele deseja apagar
     marca_apagar = questionary.select(
-        "Escolha a marca para apagar",
+        "Escolha uma marca para apagar",
         choices=opcoes,
     ).ask()
     # Abrir a conexão
     conexao = conectar()
     cursor = conexao.cursor()
     cursor.execute(f"DELETE FROM marcas WHERE nome = '" + marca_apagar + "'")
-    conexao.commit() # Efetuar a transação
-    conexao.close() # Fechar a conexão com o banco de dados
-      
-
-
-if __name__ == "__main__":
-    menu_marcas()
-
-def atualizar_marca(): # update
-    marcas = obter_todas_marcas()
-    
-    opcoes_para_escolher = []
-    for marca in marcas:
-        opcoes_para_escolher.append(marca.nome)
-        
-    marca_escolhida = questionary.select("Escolha a marca que deseja editar",
-                                         choices=opcoes_para_escolher).ask()
-    
-    nome_editado = questionary.text("Informe o nome da marca: ").ask()
-    cnpj_editado = questionary.text("Informe o CNPJ: ").ask()
-    
-    conexao = conectar()
-    cursor = conexao.cursor()
-
-
-def apagar_marca(): # delete
-    pass
-
+    conexao.commit() #efetuar a transação
+    conexao.close()
 
 if __name__ == "__main__":
     menu_marcas()
